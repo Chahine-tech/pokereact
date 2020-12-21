62c26dd7abcd2caee70c6a70d74ccaa4fd0e4d41
Chahine-tech <chahine.benahcen@gmail.com>
2020-12-21 10:08:58 +0100

    last commit

--- serveur/src/server.js
+++ serveur/src/server.js
@@ -2,11 +2,6 @@ const express = require('express')
 const app = express()
 const arg = process.argv
 
-const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`the server is listening on port ${port}`))
-
-
-
 app.listen(arg[2], function() {
     console.log(
       "Server is listening on http://localhost:"+arg[2]
