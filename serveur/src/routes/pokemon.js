75030a27d206bfcd06e625a1a29e19b036ba6530
Aymene <aymene01@hotmail.fr>
2020-12-21 13:40:38 +0100

    server final

--- serveur/src/routes/pokemon.js
+++ serveur/src/routes/pokemon.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 const pokemons = require('../data/pokemon.json')
 
-
 router.get('/', (req, res) => {
     res.json(pokemons)
 })
@@ -12,14 +11,5 @@ router.get('/:id', (req, res) => {
     if(!pokemon) return res.status(404).send('the pokemon with the given number was not found')
     res.json(pokemon)
 })
-
-
-
-router.get("/:id", (req, res) => {
-  const pokemon = pokemons.find(c => c.numero == parseInt(req.params.numero))
-    if(!pokemon) return res.status(404).send('Pokemon not found')
-
-    res.send(pokemon)
-});
 
 module.exports = router;
