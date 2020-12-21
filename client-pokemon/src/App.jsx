f60043e52397ed9c0783ff855f3a62934d0a1e39
Chahine-tech <chahine.benahcen@gmail.com>
2020-12-21 13:46:58 +0100

    last commit

--- client-pokemon/src/App.jsx
+++ client-pokemon/src/App.jsx
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.efrei.fr/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Components
         </a>
       </header>
     </div>
