a05494775fe1a7864ac1868af04412ba883ffcba
Mehdi-githu <mehdi.jabbour@efrei.net>
2020-12-21 10:37:16 +0100

    phrase

--- serveur/src/server.js
+++ serveur/src/server.js
@@ -6,6 +6,6 @@ const pokemons = require('./routes/pokemons')
 
 app.listen(arg[2], () =>  {
     console.log(
-      "Server is listening on http://localhost:"+arg[2]
+        `Server is listening on http://localhost:${argv[2]}`
     );
   });
