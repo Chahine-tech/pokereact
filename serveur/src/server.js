51f7233947abada39e3f04374eadd1448acc942b
Chahine-tech <chahine.benahcen@gmail.com>
2020-12-21 10:17:00 +0100

    last commit

--- serveur/src/server.js
+++ serveur/src/server.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const arg = process.argv
 
-app.listen(arg[2], () => {
+const pokemons = require('./routes/pokemons')
+
+app.listen(arg[2], () =>  {
     console.log(
       "Server is listening on http://localhost:"+arg[2]
     );
