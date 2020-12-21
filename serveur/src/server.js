dd19fd5936f0cf8a73513fb734b555297ddbd0fd
Aymene <aymene01@hotmail.fr>
2020-12-21 10:12:58 +0100

    modif

--- serveur/src/server.js
+++ serveur/src/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const arg = process.argv
 
-app.listen(arg[2], function() {
+app.listen(arg[2], () => {
     console.log(
       "Server is listening on http://localhost:"+arg[2]
     );
